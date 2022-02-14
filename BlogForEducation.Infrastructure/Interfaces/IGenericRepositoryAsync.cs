cd8dc98a3703b97b278e0093141ab5441d105e64
Khondamir Abdukhoshimov <60962668+iCloudBMX@users.noreply.github.com>
2022-02-14 10:47:22 +0500

    implemented Generic Repository

---
+++ BlogForEducation.Infrastructure/Interfaces/IGenericRepositoryAsync.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlogForEducation.Infrastructure.Interfaces
+{
+    public interface IGenericRepositoryAsync<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+
+        Task<IReadOnlyList<T>> GetAllAsync();
+
+        Task<IReadOnlyList<T>> GetPagedListAsync(int pageNumber, int pageSize);
+
+        Task<T> AddAsync(T entity);
+
+        Task UpdateAsync(T entity);
+        
+        Task DeleteAsync(T entity);
+    }
+}
