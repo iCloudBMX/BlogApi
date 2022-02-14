1f15b31f7cf40d11fad6f3dac5cfa905d93abb5b
Khondamir Abdukhoshimov <60962668+iCloudBMX@users.noreply.github.com>
2022-02-14 10:49:12 +0500

    implemented User Controller with crud acrions

---
+++ BlogForEducation.Api/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using BlogForEducation.Application.DTOs;
+using BlogForEducation.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BlogForEducation.Api.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            return Ok(await _userService.GetAllUsersAsync());
+        }
+
+        [HttpGet("id:int")]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            return Ok(await _userService.GetUserByIdAysnc(id));
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> GetUserById([FromBody] UserForCreationDto userDto)
+        {
+            return Created("", await _userService.CreateUserAsync(userDto));
+        }
+    }
+}
