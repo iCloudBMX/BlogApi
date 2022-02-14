21ecc9d9a08698a7cd3f5db024d22f587c231d7b
Khondamir Abdukhoshimov <60962668+iCloudBMX@users.noreply.github.com>
2022-02-14 10:46:52 +0500

    removed unnecessary namespaces

--- BlogForEducation.Domain/Models/BlogAsset.cs
+++ BlogForEducation.Domain/Models/BlogAsset.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BlogForEducation.Domain.Models
+﻿namespace BlogForEducation.Domain.Models
 {
     public class BlogAsset : BaseEntity
     {
