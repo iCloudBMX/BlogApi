851499addd3239fd38c151465b0b8e317608588e
Khondamir Abdukhoshimov <60962668+iCloudBMX@users.noreply.github.com>
2022-02-14 10:48:18 +0500

    Implemented User Service

---
+++ BlogForEducation.Application/Interfaces/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using BlogForEducation.Application.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlogForEducation.Application.Interfaces
+{
+    public interface IUserService
+    {
+        Task<IReadOnlyList<UserDto>> GetAllUsersAsync();
+
+        Task<UserDto> GetUserByIdAysnc(int id);
+
+        Task<UserDto> CreateUserAsync(UserForCreationDto user);
+    }
+}
---
+++ BlogForEducation.Application/Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using BlogForEducation.Application.DTOs;
+using BlogForEducation.Application.Interfaces;
+using BlogForEducation.Domain.Models;
+using BlogForEducation.Infrastructure.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlogForEducation.Application.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepositoryAsync _userRepository;
+        private readonly IMapper _mapper;
+
+        public UserService(IUserRepositoryAsync userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+
+            _mapper = mapper;
+        }
+
+        public async Task<UserDto> CreateUserAsync(UserForCreationDto userDto)
+        {
+            var user = _mapper.Map<User>(userDto);
+
+            return _mapper.Map<UserDto>(await _userRepository.AddAsync(user));
+        }
+
+        public async Task<IReadOnlyList<UserDto>> GetAllUsersAsync()
+        {
+            return _mapper.Map<IReadOnlyList<UserDto>>(await _userRepository.GetAllAsync());
+        }
+
+        public async Task<UserDto> GetUserByIdAysnc(int id)
+        {
+            return _mapper.Map<UserDto>(await _userRepository.GetByIdAsync(id));
+        }
+    }
+}
